b9d7a525d75c340eb1d654632de52bd9a4b2df33
Joe Bloggs <vapniks@yahoo.com>
2014-05-03 15:22:48 +0100

    update version

--- eviews.el
+++ eviews.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Joe Bloggs <vapniks@yahoo.com>
 ;; Copyleft (Ↄ) 2013, Joe Bloggs, all rites reversed.
 ;; Created: 2008/08/01 
-;; Version: 0.1
+;; Version: 0.2
 ;; Last-Updated: 2014-04-30 14:36:29
 ;;           By: Joe Bloggs
 ;; URL: https://github.com/vapniks/eviews
